5cc4c5cf3ea3fd74dfe34aed0779727b65267a7c
natalianna <35853146+natalianna@users.noreply.github.com>
2018-01-29 16:52:29 +0000

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,21 @@
+right();
+
+function a() {
+  if (getColor () == "red") {
+up();up();up();
+  } else{
+    down();down();down();
+  }
+}
+function b(){
+  if (getColor () == "blue") {
+right();up();
+  } else{
+   left();
+down(); 
+  }
+}
+a();
+b();
+up();
+up();
