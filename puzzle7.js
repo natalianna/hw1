f257cbe5de25b4f513cd1f21bce4f2d615ed0b65
natalianna <35853146+natalianna@users.noreply.github.com>
2018-01-29 16:52:51 +0000

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,25 @@
+while (getColor() != "red") {
+  down();
+}
+a();
+while (getColor() != "red") {
+  up();
+}
+a();
+while (getColor() != "red") {
+  down();
+}
+a();
+while (getColor() != "red") {
+  up();
+}
+a();
+while (getColor() != "red") {
+  down();
+}
+a(); 
+
+function a() {
+	if (getColor () == "red") {
+right();right(); 
+  } }
