efdaf4c83c1cf17bec4d3deb34f86858a5b632c1
natalianna <35853146+natalianna@users.noreply.github.com>
2018-01-29 16:56:04 +0000

    Create puzzle9.js

---
+++ puzzle9.js
@@ -0,0 +1,47 @@
+rightNineTimes();
+downNineTimes ();
+leftNineTimes ();
+upNineTimes ();
+
+function rightNineTimes () {
+  right();
+  right();right();
+  right();right();
+  right();right();
+  right();right();
+}
+
+function downNineTimes () {
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+}
+
+function leftNineTimes () {
+  left();
+  left();
+  left();
+  left();
+  left();
+  left();
+  left();
+  left();left();
+}
+
+function upNineTimes () {
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+}
