d2a514420f5b7eb3b1b48821f3aefc073fada6ad
natalianna <35853146+natalianna@users.noreply.github.com>
2018-01-29 16:53:19 +0000

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,47 @@
+downEightTimes();
+rightTwoTimes ();
+upSevenTimes ();
+rightTwoTimes ();
+downSevenTimes();
+rightTwoTimes ();
+upSevenTimes ();
+rightTwoTimes ();
+downSevenTimes();
+rightTwoTimes ();
+
+
+function downEightTimes () {
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+}
+
+function downSevenTimes () {
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+  down();
+}
+
+function rightTwoTimes () {
+  right();
+  right();
+}
+
+function upSevenTimes () {
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+}
