4dc857bc2f18b04590dfd60590a4ef4d9d8196f5
natalianna <35853146+natalianna@users.noreply.github.com>
2018-01-29 16:51:31 +0000

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,17 @@
+down();
+down();
+down();
+down();
+right();
+var A = getColor();
+down();
+setColor (A);
+right();
+right();
+up();
+right();
+var B = getColor();
+down();
+setColor (B);
+right();
+right();
